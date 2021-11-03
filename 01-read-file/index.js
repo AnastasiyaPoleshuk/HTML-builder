90194bb35bf6aadf7af654e28814b6ce7ebd79f1
AnastasiyaPoleshuk <anastasiyapaliashuk@gmail.com>
2021-11-03 13:40:49 +0300

    feat: add solution for foth task

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -3,7 +3,6 @@ let path = require('path');
 
 let readTxT = fs.ReadStream(path.join(__dirname, 'text.txt'), 'utf-8');
 
-
 readTxT.on('data', function(chunk){
     console.log(chunk);
 });
--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,25 @@
+let fs = require('fs');
 
+
+fs.mkdir('./04-copy-directory/files-copy', function() {
+    fs.readdir('./04-copy-directory/files', (err, files) => {
+        if(err){
+            console.log(err);
+        };
+
+        files.forEach(file =>{    
+            fs.stat(`./04-copy-directory/files/${file}`, (err, stats) => {
+                if(stats.size == 0){
+                    let newFile = fs.WriteStream(`./04-copy-directory/files/${file}`);
+                    newFile.write("");
+                };
+            });
+            let dataFile = fs.ReadStream(`./04-copy-directory/files/${file}`, 'utf-8');
+            dataFile.on('data', function(data){
+                fs.writeFile(`./04-copy-directory/files-copy/${file}`, data, function(){
+                    console.log('it works');
+                });
+            });
+        });
+    });
+});
